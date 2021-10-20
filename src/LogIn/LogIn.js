f22b5deb76dbd2927a53ad9bdf26b5973ef3c321
Ananna14 <https://github.com/Ananna14>
2021-10-20 17:11:25 +0600

    helth-related-website

--- src/LogIn/LogIn.js
+++ src/LogIn/LogIn.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+// import { createUserWithEmailAndPassword, Auth} from "firebase/auth";
+import { getAuth,  createUserWithEmailAndPassword} from "firebase/auth";
 import { Link, useLocation, useHistory } from 'react-router-dom'
 import useAuth from '../Hooks/useAuth'
 // import useFirebase from '../UseFirebase/UseFirebase'
 import './LogIn.css'
 
 const LogIn = () => {
+    const auth = getAuth();
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLogin, setIsLogin] = useState(false);
+
     const{signInUsingGoogle} = useAuth();
     
     const location = useLocation();
@@ -18,13 +26,37 @@ signInUsingGoogle()
 history.push(redirect_uri)
 })
     }
+
+    const toggleLogin = e =>{
+        setIsLogin(e.target.checked)
+    }
+
+    const handelEmailChange= e =>{
+        setEmail(e.target.value)
+    }
+
+    const handelPasswordChange = e =>{
+        setPassword(e.target.value)
+    }
+    
+    const handelRegistration = e =>{
+        console.log(email, password);
+        createUserWithEmailAndPassword(auth,email, password)
+        .then((result)=>{
+        const user = result.user;
+        console.log(user)
+        })
+        e.preventDefault();
+    }
     return (
         <div>
            <div className="half-width">
-           <h2>Login</h2>
-           <input type="email" className="contact" name="" id="" placeholder="Email"/><br/>
-            <input type="password" className="contact" name="" id="" placeholder="password"/><br/>
-            <button className="color">Login</button>
+           <h2 className="text-primary">Please {isLogin ? 'Login': 'Register'}</h2>
+           <input onBlur={handelEmailChange} type="email" className="contact" name="" id="" placeholder="Email"/><br/>
+            <input onBlur={handelPasswordChange} type="password" className="contact" name="" id="" placeholder="password"/><br/>
+            <input onChange={toggleLogin} type="checkbox" name="" id="" />
+            <label className="form-check-label" htmlFor="gridcheck1">Already Registered?</label><br/><br/>
+            <button className="color" onClick={handelRegistration}>{isLogin ? 'Login' : 'Register'}</button>
             <p>new to helthcare related website?<Link to="/register">Create-account</Link></p>
            </div>
             <br/>
